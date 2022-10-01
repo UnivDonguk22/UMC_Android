ef5e62964454c922b10bdaa81fc0a16519d9b709
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-10-01 14:37:34 +0900

    :sparkles: Start Android Study with Kotlin
    As First I develop Kotlin and study APP language with Android

---
+++ app/src/main/java/dev/aftermoon/umc_week2/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.aftermoon.umc_week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example1)
+    }
+}
